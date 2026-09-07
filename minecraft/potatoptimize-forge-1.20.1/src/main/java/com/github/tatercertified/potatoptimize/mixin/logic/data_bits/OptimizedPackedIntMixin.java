0933a064e0271f88839768e08283e0d9f1d899e1
Herbertofury <stealth.sanchez@hotmail.com>
2026-09-07 10:40:49 -0600

    minecraft: make packed-int optimization Forge Mixin 0.8.5 safe

--- minecraft/potatoptimize-forge-1.20.1/src/main/java/com/github/tatercertified/potatoptimize/mixin/logic/data_bits/OptimizedPackedIntMixin.java
+++ minecraft/potatoptimize-forge-1.20.1/src/main/java/com/github/tatercertified/potatoptimize/mixin/logic/data_bits/OptimizedPackedIntMixin.java
@@ -1,42 +1,50 @@
 package com.github.tatercertified.potatoptimize.mixin.logic.data_bits;
 
 import net.minecraft.util.collection.PackedIntegerArray;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * Credit to PaperMC patch #0087
+ * Credit to PaperMC patch #0087.
+ *
+ * Forge 1.20.1 ships Mixin 0.8.5, which only permits constructor callbacks at
+ * a safe post-initialization point. Cache the unsigned constants at RETURN
+ * instead of injecting at intermediate FIELD writes in the constructor.
  */
 @Mixin(PackedIntegerArray.class)
 public class OptimizedPackedIntMixin {
     @Shadow @Final private int indexScale;
     @Shadow @Final private int indexOffset;
     @Shadow @Final private int indexShift;
+
     @Unique
     @Final @Mutable
-    private long indexScaleUnsigned;
+    private long potatoptimize$indexScaleUnsigned;
+
     @Unique
     @Final @Mutable
-    private long indexOffsetUnsigned;
+    private long potatoptimize$indexOffsetUnsigned;
 
-    @Inject(method = "<init>(II[J)V", at = @At(value = "FIELD", target = "Lnet/minecraft/util/collection/PackedIntegerArray;indexOffset:I"))
-    private void addUnsigned(int elementBits, int size, long[] data, CallbackInfo ci) {
-        this.indexScaleUnsigned = Integer.toUnsignedLong(this.indexScale);
-    }
-
-    @Inject(method = "<init>(II[J)V", at = @At(value = "FIELD", target = "Lnet/minecraft/util/collection/PackedIntegerArray;indexShift:I"))
-    private void addUnsigned2(int elementBits, int size, long[] data, CallbackInfo ci) {
-        this.indexOffsetUnsigned = Integer.toUnsignedLong(this.indexOffset);
+    @Inject(method = "<init>(II[J)V", at = @At("RETURN"))
+    private void potatoptimize$cacheUnsignedIndexConstants(int elementBits, int size, long[] data, CallbackInfo ci) {
+        this.potatoptimize$indexScaleUnsigned = Integer.toUnsignedLong(this.indexScale);
+        this.potatoptimize$indexOffsetUnsigned = Integer.toUnsignedLong(this.indexOffset);
     }
 
     /**
      * @author QPCrummer
-     * @reason Simplify
+     * @reason Simplify the storage-index arithmetic while preserving vanilla output.
      */
     @Overwrite
     private int getStorageIndex(int index) {
-        return (int) ((long) index * this.indexScaleUnsigned + this.indexOffsetUnsigned >> 32 >> this.indexShift);
+        return (int) ((long) index * this.potatoptimize$indexScaleUnsigned
+                + this.potatoptimize$indexOffsetUnsigned >> 32 >> this.indexShift);
     }
 }
