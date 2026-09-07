eab7b29af0c69afe3e558eabd0518e7d8e0ecd3f
Herbertofury <stealth.sanchez@hotmail.com>
2026-09-07 10:25:20 -0600

    minecraft: retarget 1.20.1 travel-stat rounding mixin correctly

--- minecraft/potatoptimize-forge-1.20.1/src/main/java/com/github/tatercertified/potatoptimize/mixin/fastmath/rounding/FastRoundingServerPlayerMixin.java
+++ minecraft/potatoptimize-forge-1.20.1/src/main/java/com/github/tatercertified/potatoptimize/mixin/fastmath/rounding/FastRoundingServerPlayerMixin.java
@@ -1,12 +1,16 @@
 package com.github.tatercertified.potatoptimize.mixin.fastmath.rounding;
 
 import com.github.tatercertified.potatoptimize.utils.math.FasterMathUtil;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ServerPlayerEntity.class)
+/**
+ * In Minecraft 1.20.1 increaseTravelMotionStats is declared by PlayerEntity, not ServerPlayerEntity.
+ * Target the declaring class so the optional fast-rounding rule actually applies when enabled.
+ */
+@Mixin(PlayerEntity.class)
 public class FastRoundingServerPlayerMixin {
     @Redirect(
             method = "increaseTravelMotionStats(DDD)V",
